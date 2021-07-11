23f215f8088988b7a3b9c1583a052fa81b13fd04
rlipperts <rlipperts@techfak.uni-bielefeld.de>
2021-07-11 11:55:45 +0200

    Prepare project by adding main python file and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jadg",
+    version="0.0.2",
+    author="Ruben Lipperts, Georg Alberding",
+    author_email="",
+    description="Drinking game service focused on usability and extendability",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rlipperts/justanotherdrinkinggame",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Benchmark",
+    ],
+    python_requires='~=3.9',
+)
