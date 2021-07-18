d8282a0ef74aea89b92a646c6495994797e0e8cc
rlipperts <rlipperts@techfak.uni-bielefeld.de>
2021-07-18 10:50:24 +0200

    Include logging configurator in setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rlipperts/justanotherdrinkinggame",
     packages=setuptools.find_packages(exclude=("test*", "doc*")),
+    install_requires = [
+        'logging-configurator @ git+ssh://git@github.com/rlipperts/logger.git@master#egg=logging-configurator-0.0.2',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GPL-3.0 License",
         "Operating System :: OS Independent",
-        "Topic :: System :: Benchmark",
+        "Topic :: Games/Entertainment",
     ],
     python_requires='~=3.9',
 )
