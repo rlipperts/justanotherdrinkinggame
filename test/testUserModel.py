803ccc8dd383f8d61437fff0f125e21251ba30b2
galberding <galberding@techfak.uni-bielefeld.de>
2021-07-18 10:55:10 +0200

    Test random user selection

--- test/testUserModel.py
+++ test/testUserModel.py
@@ -3,17 +3,23 @@ import unittest
 
 class TestUserModel(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.user_names = ["Karl", "Ulli", "Ute"]
+        self.user_model = UserModel.from_names(self.user_names)
+
     def test_user_creation(self):
         user = User("Bob")
         self.assertEqual(user.name, "Bob")
         self.assertEqual(user.sip_counter, 0)
 
     def test_user_model(self):
-        user_names = ["Karl", "Ulli", "Ute"]
-        um = UserModel.from_names(user_names)
-        self.assertEqual(len(um.users), 3)
-        for user in um.users:
-            self.assertTrue(user.name in user_names)
+        self.assertEqual(len(self.user_model.users), 3)
+        for user in self.user_model.users:
+            self.assertTrue(user.name in self.user_names)
+
+    def test_get_random_user(self):
+        user = self.user_model.random()
+        self.assertIn(user, self.user_model.users)
 
 
 if __name__ == '__main__':
